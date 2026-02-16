ca3f424987fdbc3a7c4ff53aa5b00180d5f92ca7
Mario Finelli <mario@finel.li>
2026-02-16 20:51:43 +0100

    Add initial program documentation

--- cmd/root.go
+++ cmd/root.go
@@ -31,16 +31,16 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "modctl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "mod control: command line mod manager",
+	Long: `mod control (modctl) is a command line tool to manage game mods. It uses mod
+profiles to support deterministic install/uninstall of game mods.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+mod control (modctl)  Copyright © 2026  Mario Finelli
+This program comes with ABSOLUTELY NO WARRANTY; This program is free
+software, and you are welcome to redistribute it under certain conditions;
+You should have received a copy of the GNU General Public License (version
+3) along with this program. If not, see https://www.gnu.org/licenses/.`,
+	Version: "1.0.0",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
