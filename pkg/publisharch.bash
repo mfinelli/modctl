63d518182be1b7c0b6d9248bd605424a8612ebba
Mario Finelli <mario@finel.li>
2026-03-10 19:03:50 +0100

    Fix publish script

--- pkg/publisharch.bash
+++ pkg/publisharch.bash
@@ -22,8 +22,6 @@ echo "$RCLONE_CONFIG" > pkg/rclone.conf
 
 WORKDIR="$(mktemp -d)"
 mkdir -p "$WORKDIR/repo/arch"
-mkdir -p "$WORKDIR/repo/arch/aarch64"
-mkdir -p "$WORKDIR/repo/arch/x86_64"
 
 # we download the repo even though we could just bootstrap the repo files
 # everytime because we will eventually unify the publish scripts (when we
@@ -35,6 +33,8 @@ mkdir -p "$WORKDIR/repo/arch/x86_64"
   rclone sync --config pkg/rclone.conf r2:modctl-pkgs/arch "$WORKDIR/repo/arch"
 # fi
 
+mkdir -p "$WORKDIR/repo/arch/aarch64"
+mkdir -p "$WORKDIR/repo/arch/x86_64"
 repo-add --key pkg@modctl.org --sign "$WORKDIR/repo/arch/x86_64/modctl.db.tar.gz" "$AMD64_PKG"
 repo-add --key pkg@modctl.org --sign "$WORKDIR/repo/arch/aarch64/modctl.db.tar.gz" "$ARM64_PKG"
 
@@ -46,9 +46,14 @@ cp "$ARM64_SIG" "$WORKDIR/repo/arch/aarch64/"
 find "$WORKDIR/repo/arch" -name '*.old' -exec rm {} \;
 find "$WORKDIR/repo/arch" -name '*.old.sig' -exec rm {} \;
 
+# Dereference all symlinks so rclone can push regular files
+find "$WORKDIR/repo/arch" -type l | while read -r link; do
+    cp --remove-destination "$(readlink -f "$link")" "$link"
+done
+
 tree "$WORKDIR"
 
 # if [[ "${GITHUB_REF}" == refs/tags/v* ]]; then
   echo "Pushing updated repo to R2..."
-  rclone sync --config pkg/rclone.conf --copy-links "$WORKDIR/repo/arch" r2:modctl-pkgs/arch
+  rclone sync --config pkg/rclone.conf "$WORKDIR/repo/arch" r2:modctl-pkgs/arch
 # fi
